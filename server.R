473759b27040ab58ba7be6ffe8daa9276f59adb7
collnell <cnell@uci.edu>
2016-03-28 22:05:56 -0700

    more tabs, tables
    More or less complete variation and plasticity tabs with test results in tables. New tabs for genetic correlations and plant resistance tests with interactive checkbox inputs

--- server.R
+++ server.R
@@ -1,27 +1,59 @@
 library(shiny)##interactive
+library(reshape)
 library(datasets)##melting
 library(ggplot2)#plotting
 library(cowplot)#plot arrangment
 library(plotrix)#standard error calculations
+library(DT)
+library(ggthemes)
+set.seed(300)
+theme_set(theme_bw())##removing this creates error with the theme and doesnt put out the both water treats
+
 
 setwd("/Users/colleennell/Documents/R")
 traitlsms.water <-read.csv("/Users/colleennell/Documents/R/basa_traits/data/basa_traits_fxh20.csv")
 traitlsms.fam<- read.csv("/Users/colleennell/Documents/R/basa_traits/data/basa_traits_fx_shiny.csv")
 sexmeans<-read.csv("/Users/colleennell/Documents/R/basa_traits/data/basa_traits_sexmeansge.csv")
+GE.grow<-read.csv("/Users/colleennell/Documents/R/basa_traits/data/basa_traits_GE_grow.csv")
+sextable<-read.csv("/Users/colleennell/Documents/R/basa_traits/data/basa_traits_sextable_shiny.csv")
+SEXh2o<-read.csv("/Users/colleennell/Documents/R/basa_traits/data/basa_traits_sexh2o_shiny.csv")
+esttest<-read.csv("/Users/colleennell/Documents/R/basa_traits/data/basa_traits_estests_shiny.csv")
+
+
 traitlsms.fam$FAM<-as.factor(traitlsms.fam$FAM)
 # Define server logic required to plot various variables against
 # mpg
 shinyServer(function(input, output) {
   traitReactive <- reactive({input$trait
   })
-
+    output$GEtable <- renderDataTable({
+      datatable(GE.grow, rownames=F,filter="none",class="cell-border",options=list(dom='tr',columns.orderable="F"),
+                caption="Table 1. Results from linear mixed models testing for the effects of water, 
+                genotype, and water*genotype interaction. Block' is included in the model as a random effect)
+                to account for spatial correlation in the common garden. An interaction between 'Water' and 'Genotype' indicates genetic variation in plasticity. 
+                Model: lmer(Trait~Water*Genotype+(1|Block))")
+    })
+    output$SEXtable <- renderDataTable({
+      datatable(sextable, rownames=F,filter="none",class="cell-border",options=list(dom='tr',columns.orderable="F"),
+                caption="Table 2. ANOVA results testing for sexual dimorphism in plant traits. Analysis executed using genotype ls-means derived from raw data, and 'no water' treatment ls-means for the 'Growth' and 'Terpenes'.
+                Model: Anova(lm(Trait~Sex))")
+    })
+    output$SEXh2otable <- renderDataTable({
+      datatable(SEXh2o, rownames=F,filter="none",class="cell-border",options=list(dom='tr',columns.orderable="F"),
+                caption="Table 3. LMM results testing for sexual dimorphism in traits that showed genotype*water interaction. Analysis executed using genotype ls-means for each water treatment (2 values per genotype), and genotype included as a random effect to account for repeated measures.
+                Model: Anova(lm(Trait~Sex*Water+(1|FAM)))")
+    })
+    output$estests <- renderDataTable({
+      datatable(esttest, rownames=F,filter="none",class="cell-border",options=list(dom='tr',columns.orderable="F"),
+                caption="Table 4. ANOVA results testing for sexual dimorphism in trait plasticity for 'Growth' and 'Terpenes'. Tests were run on effect sizes calculated for each genotype genotype as a measure of plasticity.  Model: Anova(lm(plasticity~SEX))") 
+    })
   # Generate a plot of the requested variable against mpg and
   # only include outliers if requested
-    output$text1 <- renderText({ 
-      paste("Selected trait: ",input$trait)
-    })
     output$text2<-renderText({
       paste("Do the sexes differ in",input$trait)
+    })
+    output$corrplot<-renderPlot({
+      
     })
     output$famplots <- renderPlot({
       # Create long form subset of the data
@@ -35,14 +67,14 @@ shinyServer(function(input, output) {
         scale_color_brewer(palette=c("Set1"))+
         theme_bw()+
         labs(x=" Genotype (rank)",color=" ")+
-        theme(panel.border = element_blank(),axis.text.x=element_blank(),legend.position="none",text=element_text(size=18),panel.grid.major = element_blank(),panel.grid.minor = element_blank(),axis.line = element_line(size=2,color = "black"))
+        theme(panel.border = element_blank(),axis.text.x=element_blank(),legend.position="none",text=element_text(size=18, face = "plain",margin = margin(), debug = FALSE),panel.grid.major = element_blank(),panel.grid.minor = element_blank(),axis.line = element_line(size=2,color = "black"))
 
       growsex<-ggplot(traitlsms.fam,aes_string(x="SEX",y=ptrait,color="SEX"))+
         stat_summary(fun.y="mean",geom="point",size=6)+theme_bw()+
-        stat_summary(fun.data=mean_se,geom="errorbar",width=.5)+
+        stat_summary(fun.data=mean_se,geom="errorbar",width=.1)+
         scale_color_brewer(palette=c("Set1"))+
         labs(y="",x="Sex",color="Plant Sex")+
-        theme(panel.border = element_blank(),axis.text=element_blank(),text=element_text(size=18), panel.grid.major = element_blank(),panel.grid.minor = element_blank(),axis.line = element_line(size=2,color = "black"))
+        theme(panel.border = element_blank(),axis.text=element_blank(),text=element_text(size=18, face = "plain",margin = margin(), debug = FALSE), panel.grid.major = element_blank(),panel.grid.minor = element_blank(),axis.line = element_line(size=2,color = "black"))
       
       
         if ((input$trait) == "N.shoot.lsm") {
@@ -50,29 +82,29 @@ shinyServer(function(input, output) {
           growvar<-ggplot(traitlsms.fam,aes_string(x=growrank,color="SEX"))+
             theme_bw()+
             scale_color_brewer(palette=c("Set1"))+
-            theme(panel.border = element_blank(),axis.text.x=element_blank(),legend.position="none",text=element_text(size=18),panel.grid.major = element_blank(),panel.grid.minor = element_blank(),axis.line = element_line(size=2,color = "black"))
+            theme(panel.border = element_blank(),axis.text.x=element_blank(),legend.position="none",text=element_text(size=18, face = "plain",margin = margin(), debug = FALSE),panel.grid.major = element_blank(),panel.grid.minor = element_blank(),axis.line = element_line(size=2,color = "black"))
           growvar+
             scale_y_continuous(limits = c(-500,7000))+labs(y="Growth")
-          if ((input$growwater) == "Yes"){
+          if ((input$growwater) == "Water"){
             growranks=interaction(reorder(traitlsms.fam$FAM,traitlsms.fam$Y.shoot.lsm),traitlsms.fam$SEX)
             growvar<-growvar+
               geom_point(aes(x=growranks,y=Y.shoot.lsm),size=3)+
               labs(x=" Genotype (rank)",y= "Growth",color=" ")+
               geom_errorbar(aes(ymin=Y.shoot.lsm-Y.shoot.se,ymax=Y.shoot.lsm+Y.shoot.se),width=.3)
           } 
-          if ((input$growwater) == "No"){
+          if ((input$growwater) == "No water"){
             growvar<-growvar+
               geom_point(aes(y=N.shoot.lsm),size=3)+
               labs(x=" Genotype (rank)",y= "Growth",color=" ")+
               geom_errorbar(aes(ymin=N.shoot.lsm-N.shoot.se,ymax=N.shoot.lsm+N.shoot.se),width=.3)
           }
-          if ((input$growwater) == "Both"){
+          if ((input$growwater) == "Both treatments"){
             growvar<-growvar+
               geom_point(aes(y=N.shoot.lsm),shape=16,size=3)+
               geom_point(aes(y=Y.shoot.lsm),size=3,shape=1)+
               geom_errorbar(aes(ymin=Y.shoot.lsm-Y.shoot.se,ymax=Y.shoot.lsm+Y.shoot.se),width=.3)+
               geom_errorbar(aes(ymin=N.shoot.lsm-N.shoot.se,ymax=N.shoot.lsm+N.shoot.se),width=.3)+
-              labs(x=" Genotype (rank order 'no water' values)",y= "Growth",color=" ")
+              labs(x=" Genotype (ranked by 'no water' trait values)",y= "Growth",color=" ")
             growsex<-ggplot(traitlsms.water,aes(x=SEX,y=shoot.lsm,shape=WATER,color=SEX))+
               stat_summary(fun.y="mean",geom="point",size=6)+
               labs(y="",x="Sex",color="Plant Sex")+
@@ -81,7 +113,8 @@ shinyServer(function(input, output) {
               scale_color_brewer(palette=c("Set1"))+
               scale_shape_manual(values=c(16,1))+
               labs(shape="Water \nTreatment")+
-              theme(panel.border = element_blank(),axis.text=element_blank(),text=element_text(size=18), panel.grid.major = element_blank(),panel.grid.minor = element_blank(),axis.line = element_line(size=2,color = "black"))
+              theme(panel.border = element_blank(),axis.text=element_blank(),text=element_text(size=18, face = "plain",margin = margin(), debug = FALSE), panel.grid.major = element_blank(),panel.grid.minor = element_blank(),axis.line = element_line(size=2,color = "black"))
+            
           } 
       } else if ((input$trait) == "N.terps.lsm") {
         growsex<-growsex+scale_y_continuous(limits = c(-120,550))
@@ -89,28 +122,28 @@ shinyServer(function(input, output) {
           theme_bw()+
           scale_color_brewer(palette=c("Set1"))+
           scale_y_continuous(limits = c(-120,550))+labs(y="Total terpenes")+
-          theme(panel.border = element_blank(),axis.text.x=element_blank(),legend.position="none",text=element_text(size=18),panel.grid.major = element_blank(),panel.grid.minor = element_blank(),axis.line = element_line(size=2,color = "black"))
+          theme(panel.border = element_blank(),axis.text.x=element_blank(),legend.position="none",text=element_text(size=18,face = "plain",margin = margin(), debug = FALSE),panel.grid.major = element_blank(),panel.grid.minor = element_blank(),axis.line = element_line(size=2,color = "black"))
       
-        if ((input$terpswater) == "Yes"){
+        if ((input$terpswater) == "Water"){
           terpsranks=interaction(reorder(traitlsms.fam$FAM,traitlsms.fam$Y.terps.lsm),traitlsms.fam$SEX)
           growvar<-growvar+
             geom_point(aes(x=terpsranks,y=Y.terps.lsm),size=3)+
-            labs(x=" Genotype (rank)",y= "Growth",color=" ")+
+            labs(x=" Genotype (rank)",y= "Total terpenes",color=" ")+
             geom_errorbar(aes(ymin=Y.terps.lsm-Y.terps.se,ymax=Y.terps.lsm+Y.terps.se),width=.3)
         } 
-        if ((input$terpswater) == "No"){
+        if ((input$terpswater) == "No water"){
           growvar<-growvar+
             geom_point(aes(y=N.terps.lsm),size=3)+
-            labs(x=" Genotype (rank)",y= "Growth",color=" ")+
+            labs(x=" Genotype (rank)",y= "Total terpenes",color=" ")+
             geom_errorbar(aes(ymin=N.terps.lsm-N.terps.se,ymax=N.terps.lsm+N.terps.se),width=.3)
         }
-        if ((input$terpswater) == "Both"){
+        if ((input$terpswater) == "Both treatments"){
           growvar<-growvar+
             geom_point(aes(y=N.terps.lsm),shape=16,size=3)+
             geom_point(aes(y=Y.terps.lsm),size=3,shape=1)+
             geom_errorbar(aes(ymin=Y.terps.lsm-Y.terps.se,ymax=Y.terps.lsm+Y.terps.se),width=.3)+
             geom_errorbar(aes(ymin=N.terps.lsm-N.terps.se,ymax=N.terps.lsm+N.terps.se),width=.3)+
-            labs(x=" Genotype (rank order 'no water' values)",y= "Growth",color=" ")
+            labs(x=" Genotype (rank order 'no water' values)",y= "Total terpenes",color=" ")
           growsex<-ggplot(traitlsms.water,aes(x=SEX,y=terps.lsm,shape=WATER,color=SEX))+
             stat_summary(fun.y="mean",geom="point",size=6)+
             labs(y="",x="Sex",color="Plant Sex")+
@@ -119,7 +152,8 @@ shinyServer(function(input, output) {
             scale_color_brewer(palette=c("Set1"))+
             scale_shape_manual(values=c(16,1))+
             labs(shape="Water \nTreatment")+
-            theme(panel.border = element_blank(),axis.text=element_blank(),text=element_text(size=18), panel.grid.major = element_blank(),panel.grid.minor = element_blank(),axis.line = element_line(size=2,color = "black"))
+            theme(panel.border = element_blank(),axis.text=element_blank(),text=element_text(size=18, face = "plain",margin = margin(), debug = FALSE), panel.grid.major = element_blank(),panel.grid.minor = element_blank(),axis.line = element_line(size=2,color = "black"))
+          
         } 
         
         
@@ -156,26 +190,55 @@ shinyServer(function(input, output) {
         draw_plot(growvar, x = 0, y = 0, width = .75, height = 1) +
         draw_plot(growsex, x = .75, y = 0, width = .25, height = 1)
       fig2
+      
     })
+    
+    ###now plasticity plots, just for growth and terpenes (GE interact)
   output$plasticplots <- renderPlot({
-    growrankes<-interaction(reorder(traitlsms.fam$FAM,traitlsms.fam$shootes),traitlsms.fam$SEX)
-    growvares<-ggplot(traitlsms.fam,aes(x=growrankes,y=traitlsms.fam$shootes,color=traitlsms.fam$SEX))+
-      geom_point(size=2)+
-      geom_errorbar(aes(ymin=(traitlsms.fam$shootes-.1),ymax=(traitlsms.fam$shootes+.1)),width=.1)+
+    traitsub = traitlsms.fam[,c("FAM",traitReactive())]
+    traitmelt = melt(traitsub,id = "FAM")
+    pltrait = input$trait
+    
+    growranky<-interaction(reorder(traitlsms.fam$FAM,traitlsms.fam$shootes),traitlsms.fam$SEX)
+    growvary<-ggplot(traitlsms.fam,aes_string(x=growranky,y=traitlsms.fam$shootes,color="SEX"))+
+      geom_point(size=3)+
       theme_bw()+
       labs(x="Genotype (rank)",y="Growth Plasticity",color="Plant Sex")+
-      theme(legend.position="none",text=element_text(size=15),panel.grid.major = element_blank(),panel.grid.minor = element_blank())+
+      scale_color_brewer(palette=c("Set1"))+
+      theme(panel.border = element_blank(),axis.text.x=element_blank(),legend.position="none",text=element_text(size=18,face = "plain",margin = margin(), debug = FALSE),panel.grid.major = element_blank(),panel.grid.minor = element_blank(),axis.line = element_line(size=2,color = "black"))+
       scale_y_continuous(limits = c(-.6, 1.5))
-    growsexes<-ggplot(sexmeans,aes(x=sexmeans$SEX,y=sexmeans$shootESmean,color=sexmeans$SEX))+
-      geom_point(size=5)+
+    growsexy<-ggplot(sexmeans,aes(x=sexmeans$SEX,y=sexmeans$shootESmean,color=sexmeans$SEX))+
+      geom_point(size=6)+
       geom_errorbar(aes(ymin=(sexmeans$shootESmean-sexmeans$shootESse),ymax=(sexmeans$shootESmean+sexmeans$shootESse)),width=.1)+
       theme_bw()+
+      scale_color_brewer(palette=c("Set1"))+
       labs(x="Plant Sex",y=" ",color="Plant Sex")+
-      theme(axis.text.y=element_blank(),text=element_text(size=15),  panel.grid.major = element_blank(),panel.grid.minor = element_blank())+
+      theme(panel.border = element_blank(),axis.text.x=element_blank(),text=element_text(size=18, face = "plain",margin = margin(), debug = FALSE),panel.grid.major = element_blank(),panel.grid.minor = element_blank(),axis.line = element_line(size=2,color = "black"))+
       scale_y_continuous(limits = c(-.6, 1.5))
+    
+    if ((input$trait) == "N.terps.lsm") {
+    growranky<-interaction(reorder(traitlsms.fam$FAM,traitlsms.fam$terpses),traitlsms.fam$SEX)
+    growvary<-ggplot(traitlsms.fam,aes_string(x=growranky,y=traitlsms.fam$terpses,color="SEX"))+
+      geom_point(size=3)+
+      theme_bw()+
+      labs(x="Genotype (rank)",y="Terpene Plasticity",color="Plant Sex")+
+      scale_color_brewer(palette=c("Set1"))+
+      scale_y_continuous(limits = c(-2, 2))+
+      theme(panel.border = element_blank(),axis.text.x=element_blank(),legend.position="none",text=element_text(size=18, face = "plain",margin = margin(), debug = FALSE),panel.grid.major = element_blank(),panel.grid.minor = element_blank(),axis.line = element_line(size=2,color = "black"))
+    growsexy<-ggplot(traitlsms.fam,aes(x=traitlsms.fam$SEX,y=traitlsms.fam$terpses,color=traitlsms.fam$SEX))+
+      stat_summary(fun.y="mean",geom="point",size=6)+
+      stat_summary(fun.data=mean_se,geom="errorbar",width=.1)+
+      theme_bw()+
+      scale_color_brewer(palette=c("Set1"))+
+      labs(x="Plant Sex",y=" ",color="Plant Sex")+
+      scale_y_continuous(limits = c(-2, 2))+
+      theme(panel.border = element_blank(),axis.text.x=element_blank(),text=element_text(size=18, face = "plain",margin = margin(), debug = FALSE),panel.grid.major = element_blank(),panel.grid.minor = element_blank(),axis.line = element_line(size=2,color = "black"))
+    
+    }
+  
     fig4 <- ggdraw() + 
-      draw_plot(growvares, x = 0, y = 0, width = .75, height = 1) +
-      draw_plot(growsexes, x = .75, y = 0, width = .25, height = 1)
+      draw_plot(growvary, x = 0, y = 0, width = .75, height = 1) +
+      draw_plot(growsexy, x = .75, y = 0, width = .25, height = 1)
     fig4
   })
 })
--- ui.R
+++ ui.R
@@ -3,32 +3,76 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("BASA G*E tests"),
+  titlePanel("BASA trait variation analyses"),
   sidebarLayout(
     sidebarPanel(
-      selectInput("trait", "Plant trait:",
-                  list("Growth" = "N.shoot.lsm","Terpenes" = "N.terps.lsm","SLA"="SLA.lsm","%H20"="H20.lsm","leaf toughness"="tough.lsm.x","CN"="CN.lsm","WUE"="WUE.lsm","flowers"="flower.lsm","Resistance"="resist.lsm","%N"="perN.lsm")),
       conditionalPanel(
-        condition="input.trait=='N.shoot.lsm'",
-        radioButtons("growwater","Water Treatment",choices=c("Yes","No","Both"),selected="No")
+        condition="input.tabs=='Trait variation' || input.tabs=='Plasticity'",
+          selectInput("trait", "Select plant trait to plot:",
+                      list("Growth" = "N.shoot.lsm","Terpenes" = "N.terps.lsm","SLA"="SLA.lsm","%H20"="H20.lsm","leaf toughness"="tough.lsm.x","CN"="CN.lsm","WUE"="WUE.lsm","flowers"="flower.lsm","Resistance"="resist.lsm","%N"="perN.lsm"))
       ),
+        conditionalPanel(
+          condition="input.trait=='N.shoot.lsm' && input.tabs=='Trait variation'",
+          radioButtons("growwater","Pick treatment to plot:",choices=c("Water","No water","Both treatments"),selected="No water")
+        ),
+        conditionalPanel(
+          condition="input.trait=='N.terps.lsm' && input.tabs=='Trait variation'",
+          radioButtons("terpswater","Pick treatment to plot:",choices=c("Water","No water","Both treatments"),selected="No water")
+        ),
       conditionalPanel(
-        condition="input.trait=='N.terps.lsm'",
-        radioButtons("terpswater","Water Treatment",choices=c("Yes","No","Both"),selected="Both")
+        condition="input.tabs=='Trade-offs'",
+        checkboxGroupInput("trade","Select traits for correlations:",choices=c("Growth" = "N.shoot.lsm","Terpenes" = "N.terps.lsm","SLA"="SLA.lsm","%H20"="H20.lsm","leaf toughness"="tough.lsm.x","CN"="CN.lsm","WUE"="WUE.lsm","flowers"="flower.lsm","Resistance"="resist.lsm","%N"="perN.lsm"))
       )
     ),
 
     mainPanel(
-      tabsetPanel(
-        tabPanel("Genetic Variation",
+      tabsetPanel(id="tabs",
+        tabPanel("Trait variation",
                  br(),
-                 h4(textOutput("text1")),
+                 h4("Do plant traits vary by genotype and watering treatment?"),
                  br(),
-                 plotOutput("famplots",height="300",width="800")
+                 plotOutput("famplots",height="300",width="1000"),
+                 br(),
+                 br(),
+                 dataTableOutput("GEtable"),
+                 br(),
+                 br(),
+                 dataTableOutput("SEXtable"),
+                 br(),
+                 br(),
+                 dataTableOutput("SEXh2otable"),
+                 br(),
+                 br(),
+                 p("All mixed models were run using 'lmer' in 'lme4' package. P-values are calculated from 10,000 parametric bootstrap simulations of the deviance statistic for each fixed effect term in the model (package 'afex').")
         ),
         tabPanel("Plasticity",
-                 plotOutput("plasticplots",height="300",width="800"))
+                 br(),
+                 h4("Is there genetic variation in trait plasticity?"),
+                 br(),
+                 plotOutput("plasticplots",height="300",width="1000"),
+                 br(),
+                 br(),
+                 h4("Trait plasticity = ln(water treatment/no water)"),
+                 br(),
+                 dataTableOutput("estests",width="400"),
+                 br(),
+                 br()
         
+        ),
+        tabPanel("Trade-offs",
+                 br(),
+                 h4("How are BASA traits correlated?"),
+                 br(),
+                 plotOutput("corrplot"),
+                 br()
+        ),
+        tabPanel("Resistance",
+                 br(),
+                 h4("Does genetic variation in traits influence plant resistance?"),
+                 br(),
+                 plotOutput("resistplot"),
+                 br()
+        )
       )
     )
   )
